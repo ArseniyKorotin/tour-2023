d4c8ecbdf0f1ef7a05c704d15a39f4360d409a79
miwanoff <marina.iva.noffa@gmail.com>
2023-10-21 15:24:02 +0300

    Add header ahd footer

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Tour agency!</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid p-5 bg-primary text-white text-center">
+        <h1>Tour agency!</h1>
+        <p>Choose your journey!</p>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
